74ed4fc583cc97cfa8c563855c5f40e07f9f08fc
seungwookim <tmddno1@naver.com>
2016-11-02 02:40:44 +0000

    fix contrib test code

--- chap18_skflow_cnn.py
+++ chap18_skflow_cnn.py
@@ -40,13 +40,11 @@ def conv_model(X, y):
     X = tf.reshape(X, [-1, 28, 28, 1])
     # first conv layer will compute 32 features for each 5x5 patch
     with tf.variable_scope('conv_layer1'):
-        h_conv1 = learn.ops.conv2d(X, n_filters=32, filter_shape=[5, 5],
-                                   bias=True, activation=tf.nn.relu)
+        h_conv1 = tf.contrib.layers.conv2d(X, num_outputs=32, kernel_size=[5, 5],activation_fn=tf.nn.relu)
         h_pool1 = max_pool_2x2(h_conv1)
     # second conv layer will compute 64 features for each 5x5 patch.
     with tf.variable_scope('conv_layer2'):
-        h_conv2 = learn.ops.conv2d(h_pool1, n_filters=64, filter_shape=[5, 5],
-                                   bias=True, activation=tf.nn.relu)
+        h_conv2 = tf.contrib.layers.conv2d(h_pool1, num_outputs=64, kernel_size=[5, 5], activation_fn=tf.nn.relu)
         h_pool2 = max_pool_2x2(h_conv2)
         # reshape tensor into a batch of vectors
         h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
